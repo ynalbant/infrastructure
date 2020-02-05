04c5ec7ddd42dd4507adf6b06fca985c42c1df75
farrukh90 <farrukhsadykov@gmail.com>
2020-02-05 13:03:32 -0600

    asdfasdf

--- jenkins/iam/Jenkinsfile.groovy
+++ jenkins/iam/Jenkinsfile.groovy
@@ -2,10 +2,4 @@ node {
     stage("Clone Repo"){
         git 'https://github.com/farrukh90/iam.git'
     }
-    stage("Get list of User"){
-        readFile 'userlist'
-        env.WORKSPACE = pwd()
-        def file = readFile "${env.WORKSPACE}/userlist"    
-        file.split("\n")
-    }
 }
