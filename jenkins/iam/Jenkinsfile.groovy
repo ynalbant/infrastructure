20d10e64f063f7e8b4ecb95da3f1a89b81c7904b
farrukh90 <farrukhsadykov@gmail.com>
2020-02-05 12:47:28 -0600

    added read line

--- jenkins/iam/Jenkinsfile.groovy
+++ jenkins/iam/Jenkinsfile.groovy
@@ -2,4 +2,9 @@ node {
     stage("Clone Repo"){
         git 'https://github.com/farrukh90/iam.git'
     }
+    stage("Get list of User"){
+        new File("userlist").eachLine { line ->
+            println line
+        }
+    }
 }
