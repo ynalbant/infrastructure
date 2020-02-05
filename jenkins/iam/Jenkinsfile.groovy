e7669cca91f8847de3425fd91a4a204a023f3701
farrukh90 <farrukhsadykov@gmail.com>
2020-02-05 12:52:07 -0600

    added ./

--- jenkins/iam/Jenkinsfile.groovy
+++ jenkins/iam/Jenkinsfile.groovy
@@ -3,7 +3,7 @@ node {
         git 'https://github.com/farrukh90/iam.git'
     }
     stage("Get list of User"){
-        dh = new File('userlist')
+        dh = new File('./userlist')
             dh.eachFile {
             println(it)
         }
