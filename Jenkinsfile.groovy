3210c6efca4b7acf5bc838be89e038ea6adabeb3
farrukh90 <farrukhsadykov@gmail.com>
2020-02-05 20:01:30 -0600

    removed property

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 node {
-    properties([pipelineTriggers([cron('* * * * *')])])
+    //properties([pipelineTriggers([cron('* * * * *')])])
     stage("Pull Repo"){
         //This line pulls a repo
         git 'https://github.com/farrukh90/packer.git'    
@@ -11,9 +11,16 @@ node {
         //input 'Deploy to QA? '
         //sleep 3
     }
+    stage("Perform Some Other Task"){
+        //This will call another job
+        //build "Item1"
+    }
     stage("Deploy to QA"){
         echo "Hello World"
     }
+    stage("Perform task on Husein Machine")
+        // Performs task on remote system
+        //sh "curl http://jenkins_master.acirrustech.com:8080/job/Item1/build?token=ITEM1"
     stage("Script"){
 		sh label: '', script: 
 		'''#!/bin/bash
@@ -30,9 +37,16 @@ node {
 			echo "hello"
 		}
 	}
+    stage("Failure"){
+        //error 'Intentional Failure'
+    }
+
     stage("Send Notification to slack"){
         echo "Hello World"
         slackSend channel: 'nagios_alerts', message: 'Completed'
         mail bcc: 'farrukh@gmail.com', body: 'message', cc: 'farrukh@gmail.com', from: '', replyTo: '', subject: 'Completed', to: 'farrukh@gmail.com'
+    }
+    stage("Clean WS"){
+        cleanWs()
     }
 }
