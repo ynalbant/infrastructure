40b9960a496b36b2a076f2d0b72ad7b144ad7e1e
farrukh90 <farrukhsadykov@gmail.com>
2020-02-05 18:41:05 -0600

    added more stages

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,14 @@
+node {
+    stage("Stage1"){
+        echo "Hello World"
+    }
+    stage("Stage2"){
+        echo "Hello World"
+    }
+    stage("Stage3"){
+        echo "Hello World"
+    }
+    stage("Stage4"){
+        echo "Hello World"
+    }
+}
--- jenkins/iam/Jenkinsfile.groovy
+++
@@ -1,5 +0,0 @@
-node {
-    stage("Clone Repo"){
-        git 'https://github.com/farrukh90/iam.git'
-    }
-}
