39a7937a67396f33eabd445154c7f27918093149
farrukh90 <farrukhsadykov@gmail.com>
2020-02-13 20:06:39 -0600

    changed order

--- awx/awx.tf
+++ awx/awx.tf
@@ -1,13 +1,13 @@
 module "awx" {
   source            = "./modules"
+  ami               = "${var.ami}"
+  user              = "${var.user}"
   domain            = "${var.domain}"
   region            = "${var.region}"
-  key_name          = "${var.key_name}"
-  user              = "${var.user}"
-  ssh_key_location  = "${var.ssh_key_location}"
-  zone_id           = "${var.zone_id}"
-  ami               = "${var.ami}"
   vpc_id            = "${var.vpc_id}"
+  zone_id           = "${var.zone_id}"
+  key_name          = "${var.key_name}"
   instance_type     = "${var.instance_type}"
+  ssh_key_location  = "${var.ssh_key_location}"
 }
 
