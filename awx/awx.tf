f56e968fe5fdcb85c3e1c95dc5fc0288a6d96282
farrukh90 <farrukhsadykov@gmail.com>
2020-02-13 20:04:50 -0600

    added more options

--- awx/awx.tf
+++ awx/awx.tf
@@ -1,8 +1,13 @@
 module "awx" {
-  source = "./modules"
-  domain = "${var.domain}"
-  region = "${var.region}"
-  key_name = "${var.key_name}"
-  user = "${var.user}"
+  source            = "./modules"
+  domain            = "${var.domain}"
+  region            = "${var.region}"
+  key_name          = "${var.key_name}"
+  user              = "${var.user}"
+  ssh_key_location  = "${var.ssh_key_location}"
+  zone_id           = "${var.zone_id}"
+  ami               = "${var.ami}"
+  vpc_id            = "${var.vpc_id}"
+  instance_type     = "${var.instance_type}"
 }
 
