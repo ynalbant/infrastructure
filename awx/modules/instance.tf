aa913e2a66b1d88011933d8a8e27f88d01db4bf4
root <root@ip-172-31-25-164.eu-west-2.compute.internal>
2020-02-15 13:56:56 +0000

    added more filres

---
+++ awx/modules/instance.tf
@@ -0,0 +1,50 @@
+resource "aws_instance" "workers" {
+  instance_type               = "${var.instance_type}"
+  key_name                    = "${var.key_name}"
+  ami                         = "${data.aws_ami.centos.id}"
+  associate_public_ip_address = "true"
+  security_groups             = ["allow_ssh_and_awx"]
+  
+
+  provisioner "file" {
+    source      = "/root/.ssh"
+    destination = "/tmp/"
+
+    connection {
+      host        = "${self.public_ip}"
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.ssh_key_location)}"
+    }
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      host        = "${self.public_ip}"
+      type        = "ssh"
+      user        = "${var.user}"
+      private_key = "${file(var.ssh_key_location)}"
+    }
+
+    inline = [
+      "sudo yum install -y epel-release",
+      "sudo yum install -y yum-utils device-mapper-persistent-data lvm2 ansible git python-devel python-pip python-docker-py vim-enhanced ",
+      "sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo",
+      "sudo yum install  docker-ce -y",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo pip uninstall docker docker-py docker-compose",
+      "sudo pip install docker-compose==1.9",
+      "sudo useradd -G wheel,docker ansible", 
+ 
+
+      "# These commands below used for disabling host key verification",
+      "sudo mv /tmp/.ssh /home/ansible/ &> /dev/null",
+      "sudo cp /tmp/.ssh/id_rsa.pub /home/ansible/.ssh/authorized_keys  &> /dev/null",
+      "sudo chmod 600 /home/ansible/.ssh/authorized_keys",
+      "sudo chown -R ansible:ansible /home/ansible/",
+      "sudo chmod 0600 /home/ansible/.ssh/id_rsa",       
+     
+    ]
+  }
+}
