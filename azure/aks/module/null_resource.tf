1c15372b8e4cb10f38d80e59c39bd6a380b8a903
farrukh90 <farrukhsadykov@gmail.com>
2020-03-20 16:39:14 +0000

    added depends

--- azure/aks/module/null_resource.tf
+++ azure/aks/module/null_resource.tf
@@ -1,4 +1,8 @@
 resource "null_resource" "authenticate" {
+    depends_on  =   [
+        azurerm_kubernetes_cluster.cluster,
+        azurerm_resource_group.example
+    ]
     provisioner "local-exec" {
         command = "az aks get-credentials --resource-group example-resources --name example-aks1"
     }
@@ -7,6 +11,9 @@ resource "null_resource" "authenticate" {
 
 
 resource "null_resource" "nodes" {
+    depends_on = [
+        null_resource.authenticate
+    ]
     provisioner "local-exec" {
         command = "kubectl get nodes"
     }
