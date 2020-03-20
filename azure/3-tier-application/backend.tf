339266b54ab45497f260d7cbce419f0d4a1a4cae
farrukh90 <farrukhsadykov@gmail.com>
2020-03-20 10:11:58 -0500

    Update backend.tf

--- azure/3-tier-application/backend.tf
+++ azure/3-tier-application/backend.tf
@@ -4,7 +4,7 @@ terraform {
     resource_group_name       = "dev"
     storage_account_name      = "dev1farrukh"
     container_name            = "devcontainer"
-    access_key                = "lzyWHMGzFEzPDIV0OxUPiTA0WxYzSX6fHm4tMe9jMZwHPSn0GV5U3EZoNGiyuPkylXnzdMmORpzEMFu45OXzZQ=="
+    access_key                = ""
     key                       = "dev_terraform.tfstate"
   }
 }
